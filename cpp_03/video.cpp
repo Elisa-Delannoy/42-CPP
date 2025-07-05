301d5eae0df3944c05d5ac99bd534af69c55e399
edelannoperso <elisadelannoy10@gmail.com>
2025-07-05 12:34:24 +0200

    video

---
+++ cpp_03/video.cpp
@@ -0,0 +1,11 @@
+
+
+virtual  : linkage dynamique, a l execution le compilateur regarde le type reel de l objet
+fonction membre virtuelle = une methode 
+
+
+vitural fonciton = 0 -> methode pure : on ne peut pas l implementer, classe abstraite
+on ne peut pas l utiliser dans la classe mere mais on peut l utiliser dans els classes filles si la fonciton est implementée
+
+interface : uniquement des meyhodes pures et aucun attribut
+c est une maniere de definir un comportement pour les objets
